606510d2070aecf7d022c2fba5b62c145f32bb75
German Encina <gencina2@gmail.com>
2023-06-14 22:43:44 -0400

    Create personal exercise 1

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = this;
+        EditText etNombre = findViewById(R.id.etNombre);
+        Button btnSaludo = findViewById(R.id.btnSaludo);
+        btnSaludo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence text = etNombre.getText().toString();
+
+                Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG) ;
+                toast.show();
+
+            }
+        });
     }
+
 }
